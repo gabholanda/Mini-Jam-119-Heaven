af6a380c3279bcf3ef933b8e9be66ed77efd3e30
Gabriel Holanda <gabriel_ebot@hotmail.com>
2022-11-11 04:41:06 -0400

    Added New Input System, Input Reader SO and initial Player Controller

---
+++ Assets/Code/InputReader.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[CreateAssetMenu(fileName = "New Input Reader", menuName = "Scriptable Objects/Input Reader")]
+public class InputReader : ScriptableObject
+{
+    public InputAction Move;
+    public InputAction Attack;
+}
---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputReader reader;
+
+    private void OnEnable()
+    {
+        reader.Move.performed += OnMove;
+        reader.Attack.performed += OnAttack;
+        reader.Move.Enable();
+        reader.Attack.Enable();
+    }
+
+    private void OnDisable()
+    {
+        reader.Move.performed -= OnMove;
+        reader.Attack.performed -= OnAttack;
+        reader.Move.Disable();
+        reader.Attack.Disable();
+    }
+
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log(context.ReadValue<Vector2>());
+    }
+
+    private void OnAttack(InputAction.CallbackContext context)
+    {
+        Debug.Log("I am attacking :)");
+    }
+}
