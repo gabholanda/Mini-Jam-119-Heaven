46758f450a81bfb51f061eba218ea89095376dcb
Gabriel Holanda <gabriel_ebot@hotmail.com>
2022-11-11 22:59:50 -0400

    Limitations on stats

--- Assets/Code/CharacterStats.cs
+++ Assets/Code/CharacterStats.cs
@@ -5,14 +5,17 @@ public class CharacterStats : MonoBehaviour
     private int currentHealth;
     public int CurrentHealth { get { return currentHealth; } set { currentHealth = value; } }
 
+    [Min(1)]
     [SerializeField]
     private int maxHealth;
     public int MaxHealth { get { return maxHealth; } set { maxHealth = value; } }
 
+    [Min(0)]
     [SerializeField]
     private int damage;
     public int Damage { get { return damage; } set { damage = value; } }
 
+    [Min(0)]
     [SerializeField]
     private float speed;
     public float Speed { get { return speed; } set { speed = value; } }
