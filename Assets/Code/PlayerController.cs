fa4611b147e2907a3444c5b3896e8739a8d50cd1
yunigay <aionnkm@gmail.com>
2022-11-11 23:12:51 -0500

    Dashing code

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -9,44 +9,65 @@ public class PlayerController : MonoBehaviour
     public Transform point;
     public float radius = 1;
     public IMovable movable;
-    
+    public Vector2 direction;
+    public float dashCooldown;
+    public float dashDuration;
+    private bool canDash = true;
+
     void Awake()
     {
         movable = GetComponent<IMovable>();
     }
-    
+
     private void OnEnable()
     {
+        reader.Attack.performed += OnAttack;
+        reader.Dash.performed += OnDash;
         reader.Move.performed += OnMove;
         reader.Move.canceled += OnStopMove;
-        reader.Attack.performed += OnAttack;
         reader.Move.Enable();
         reader.Attack.Enable();
+        reader.Dash.Enable();
     }
+
 
     private void OnDisable()
     {
         reader.Move.performed -= OnMove;
         reader.Attack.performed -= OnAttack;
         reader.Move.canceled -= OnStopMove;
+        reader.Dash.performed -= OnDash;
         reader.Move.Disable();
         reader.Attack.Disable();
+        reader.Dash.Disable();
     }
 
+    private void OnDash(InputAction.CallbackContext context)
+    {
+        if (canDash)
+        {
+            canDash = false;
+            movable.SetVector(direction * 10);
+            StartCoroutine(CoolDown());
+            StartCoroutine(DashingDuration());
+        }
+
+
+
+    }
 
     private void OnMove(InputAction.CallbackContext context)
     {
-        movable.SetVector(context.ReadValue<Vector2>());
-        Debug.Log(context.ReadValue<Vector2>());
+        direction = context.ReadValue<Vector2>();
+        movable.SetVector(direction);
     }
     private void OnStopMove(InputAction.CallbackContext context)
     {
         movable.SetVector(new Vector2(0, 0));
-        Debug.Log(context.ReadValue<Vector2>());
     }
     private void OnAttack(InputAction.CallbackContext context)
     {
-       Collider2D[] enemies = Physics2D.OverlapCircleAll(point.position, radius);
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(point.position, radius);
         for (int i = 0; i < enemies.Length; i++)
         {
             // TODO: attack our enemies witin the range
@@ -58,4 +79,18 @@ public class PlayerController : MonoBehaviour
     {
         Gizmos.DrawSphere(point.position, radius);
     }
+
+    IEnumerator CoolDown()
+    {
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+
+    IEnumerator DashingDuration()
+    {
+        yield return new WaitForSeconds(dashDuration);
+        movable.SetVector(direction);
+    }
+
 }
+
