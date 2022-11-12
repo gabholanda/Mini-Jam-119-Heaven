b01e31b807e1d843cce4bc72237248493e1001c0
Gabriel Holanda <gabriel_ebot@hotmail.com>
2022-11-12 19:58:44 -0400

    Power Up System

---
+++ Assets/Code/PowerUp System/PowerUp.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PowerUp : MonoBehaviour
+{
+    public Animator animator;
+    public Vector2 position;
+    public PowerUpStats powerUpStats;
+    public UnityEvent OnEquipEvent;
+
+    private void Awake()
+    {
+        OnEquip();
+        transform.localPosition = position;
+    }
+
+    public void OnEquip()
+    {
+        CharacterStats playerStats = GetComponentInParent<CharacterStats>();
+
+        if (powerUpStats.isDamageFlat)
+        {
+            playerStats.Damage += (int)powerUpStats.damage;
+        }
+        else
+        {
+            float newDamage = playerStats.Damage * (powerUpStats.damage + 1.0f);
+            playerStats.Damage *= (int)newDamage;
+        }
+        if (powerUpStats.isHealthFlat)
+        {
+            float newHealth = playerStats.Damage * (powerUpStats.damage + 1.0f);
+            playerStats.MaxHealth *= (int)newHealth;
+        }
+        if (powerUpStats.isSpeedFlat)
+        {
+            float newSpeed = playerStats.Damage * (powerUpStats.damage + 1.0f);
+            playerStats.Speed *= newSpeed;
+        }
+        OnEquipEvent?.Invoke();
+    }
+
+    protected void OnDestroy()
+    {
+        // TODO: Give the amount of angel feathers to the player
+    }
+}
