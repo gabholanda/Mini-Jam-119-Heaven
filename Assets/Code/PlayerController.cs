8d4d17bf515948aefb9ff0e665ce7fe4b2647030
BGPlayz1 <brettgarcia9@hotmail.com>
2022-11-12 00:56:20 -0500

    Particle and kill

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public float radius = 1;
     public IMovable movable;
     public CharacterStats characterStats;
+    [SerializeField] ParticleSystem hitParticle = null; 
     
     void Awake()
     {
@@ -39,22 +40,25 @@ public class PlayerController : MonoBehaviour
     private void OnMove(InputAction.CallbackContext context)
     {
         movable.SetVector(context.ReadValue<Vector2>());
-        Debug.Log(context.ReadValue<Vector2>());
     }
     private void OnStopMove(InputAction.CallbackContext context)
     {
         movable.SetVector(new Vector2(0, 0));
-        Debug.Log(context.ReadValue<Vector2>());
     }
     private void OnAttack(InputAction.CallbackContext context)
     {
        Collider2D[] enemies = Physics2D.OverlapCircleAll(point.position, radius);
         for (int i = 0; i < enemies.Length; i++)
         {
-            Debug.Log(enemies[i].name);
             enemies[i].GetComponent<CharacterStats>().CurrentHealth -= characterStats.Damage;
-            Debug.Log(enemies[i].GetComponent<CharacterStats>().CurrentHealth);
+            Hit();
         }
+    }
+
+    public void Hit()
+    {
+        hitParticle.Play();
+
     }
 
     private void OnDrawGizmos()
