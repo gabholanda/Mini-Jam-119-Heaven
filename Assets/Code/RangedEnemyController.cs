6e8d1dd0c6ef7f23902bbb40c7b56bd61d8e1560
Gabriel Holanda <gabriel_ebot@hotmail.com>
2022-11-13 16:08:11 -0400

    First boss

--- Assets/Code/RangedEnemyController.cs
+++ Assets/Code/RangedEnemyController.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class RangedEnemyController : EnemyController
 {
-
-
     public void FireBeam()
     {
         realTrigger.Fire(transform.position, player.transform.position);
