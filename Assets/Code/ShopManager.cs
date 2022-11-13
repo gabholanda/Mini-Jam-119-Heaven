6d1b34d05f683500ffb8292b22b99b705e2b05c4
yunigay <aionnkm@gmail.com>
2022-11-12 19:53:18 -0500

    Item AKA shop system

--- Assets/Code/ShopManager.cs
+++ Assets/Code/ShopManager.cs
@@ -6,9 +6,31 @@ using UnityEngine.UI;
 public class ShopManager : MonoBehaviour
 {
 
-    public GameObject Item1;
-    public GameObject Item2;
-    public GameObject Item3;
-    public GameObject Item4;
+    public List<ItemSO> allItems;
+
+    public GameObject Item1Pos;
+    public GameObject Item2Pos;
+    public GameObject Item3Pos;
+    public GameObject Item4Pos;
+
+    private void Awake()
+    {
+        ItemSO firstRandom = allItems[Random.Range(0, allItems.Count)];
+        allItems.Remove(firstRandom);
+        Instantiate(firstRandom.itemPrefab, Item1Pos.transform.position, Quaternion.identity);
+
+        ItemSO secondRandom = allItems[Random.Range(0, allItems.Count)];
+        allItems.Remove(secondRandom);
+        Instantiate(secondRandom.itemPrefab, Item2Pos.transform.position, Quaternion.identity);
+
+        ItemSO thirdRandom = allItems[Random.Range(0, allItems.Count)];
+        allItems.Remove(thirdRandom);
+        Instantiate(thirdRandom.itemPrefab, Item3Pos.transform.position, Quaternion.identity);
+
+        ItemSO fourhtRandom = allItems[Random.Range(0, allItems.Count)];
+        Instantiate(fourhtRandom.itemPrefab, Item4Pos.transform.position, Quaternion.identity);
+
+
+    }
 
 }
